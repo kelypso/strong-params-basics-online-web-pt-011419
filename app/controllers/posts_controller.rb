9cc6415a5bc9cee8ad2b2fd37e6a266192af9a8a
Kelsey Low <kklowlow@gmail.com>
2019-06-05 03:18:47 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,22 +12,22 @@ class PostsController < ApplicationController
 	end
 
   def create
-  @post = Post.new(post_params(:title, :description))
-  @post.save
-  redirect_to post_path(@post)
-end
+    @post = Post.new(post_params(:title, :description))
+    @post.save
+    redirect_to post_path(@post)
+  end
  
-def update
-  @post = Post.find(params[:id])
-  @post.update(post_params(:title))
-  redirect_to post_path(@post)
-end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params(:title))
+    redirect_to post_path(@post)
+  end
  
-private
+  private
  
-def post_params
-  params.require(:post).permit(:title, :description)
-end
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 
 	def edit
 	  @post = Post.find(params[:id])
